19dee4bf4b1857a21831463a6ec567c6f821e077
Cbutler1 <collin.p.butler1@gmail.com>
2022-06-29 13:43:17 -0400

    first commit for basic loops

---
+++ BasicLoopsButler/Program.cs
@@ -0,0 +1,68 @@
+﻿//string input;
+
+//do
+//{
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Would you like to continue (y/n)?");
+//    input = Console.ReadLine().ToLower();
+//} while (input == "y");
+
+//needs to count up as well 
+//Console.WriteLine("Enter a number: ");
+//int i = int.Parse(Console.ReadLine());
+//for (int j = i; j >= 0; j--)
+//{
+//    Console.WriteLine(j);
+//    if (j == 0)
+//    {
+//        break;
+//    }
+//}
+//for (int x = 0; x <= i; x++)
+//{
+//    Console.WriteLine(x);
+//    if (i == x)
+//    {
+//        break;
+//    }
+//}
+
+//while loop keypad 
+//int attempt = 0;
+//while (attempt < 6)
+//{
+
+//    Console.WriteLine("Enter Code");
+//    int code = int.Parse(Console.ReadLine());
+//    attempt++;
+//    if (code == 13579)
+//    {
+//        Console.WriteLine("Welcome Home!");
+//            break;
+//    }
+//    if (attempt == 5)
+//    {
+//        Console.WriteLine("Sorry, please wait to try again.  Too many incorrect attempts.");
+//        break;
+//    }
+
+//}
+
+//do while loop keypad
+int attempts = 0;
+do
+{
+    Console.WriteLine("Enter Code");
+    int code = int.Parse(Console.ReadLine());
+    attempts++;
+    if (code == 13579)
+    {
+        Console.WriteLine("Welcome Home!");
+        break;
+    }
+} while (attempts < 5);
+Console.WriteLine("Too any unsuccessful attempts, please try again later.");
+
+
+
+
